d6a0b1fbea821e98c6367ba4131a2c116a26db91
Andrew Fasano <fasano@mit.edu>
2022-11-22 09:44:41 -0500

    Build script for release tarballs

---
+++ setup_all.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -eux
+
+#rm -rf kernels
+#mkdir kernels
+#
+#./setup.sh arm
+#./setup.sh mipsel
+#./setup.sh mipseb
+#
+#cp build/arm/arch/arm/boot/zImage kernels/zImage.armel
+#cp build/arm/vmlinux kernels/vmlinux.armel
+#
+#cp build/mipseb/vmlinux kernels/vmlinux.mipseb
+#cp build/mipsel/vmlinux kernels/vmlinux.mipsel
+
+PROF=kernels/firmadyne_profiles.conf
+echo "[arm]" > $PROF
+cat panda_profile.arm* >> $PROF
+echo "[mipseb]" > $PROF
+cat panda_profile.mipseb >> $PROF
+echo "[mipsel]" >> $PROF
+cat panda_profile.mipsel >> $PROF
+
+cp console.bins/* kernels/
+
+echo "Built by $(whoami) on $(date) at version $(git describe HEAD)" > kernels/README.txt
