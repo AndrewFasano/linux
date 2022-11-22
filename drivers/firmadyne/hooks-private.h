1169c8872a6bdd0bb8d802f3332fab8646267c57
Andrew Fasano <fasano@mit.edu>
2022-11-21 22:32:46 -0500

    Simplify/expand firmadyne jprobes, get filenames and bind info

--- drivers/firmadyne/hooks-private.h
+++ drivers/firmadyne/hooks-private.h
@@ -1,13 +1,6 @@
 #ifndef INCLUDE_PRIVATE_H
 #define INCLUDE_PRIVATE_H
 
-/* from net/bridge/br_private.h */
-struct net_bridge {
-	spinlock_t                      lock;
-	struct list_head                port_list;
-	struct net_device               *dev;
-};
-
 /* for fs/exec.c */
 static int count(const char __user * const __user *argv, int max) {
 	int i = 0;
