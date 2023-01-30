876a2ee552cf3df2b9d725b6ec1eef9ad0d16ad4
Andrew Fasano <fasano@mit.edu>
2023-01-30 13:28:49 -0500

    Refactor build scripts for docker

--- setup_all.sh
+++ setup_all.sh
@@ -15,14 +15,12 @@ cp build/arm/vmlinux kernels/vmlinux.armel
 cp build/mipseb/vmlinux kernels/vmlinux.mipseb
 cp build/mipsel/vmlinux kernels/vmlinux.mipsel
 
-PROF=kernels/firmadyne_profiles.conf
-echo "[armel]" > $PROF
-cat panda_profile.arm >> $PROF
-echo "[mipseb]" >> $PROF
-cat panda_profile.mipseb >> $PROF
-echo "[mipsel]" >> $PROF
-cat panda_profile.mipsel >> $PROF
-
-cp console.bins/* kernels/
-
+#PROF=kernels/firmadyne_profiles.conf
+#echo "[armel]" > $PROF
+#cat panda_profile.arm >> $PROF
+#echo "[mipseb]" >> $PROF
+#cat panda_profile.mipseb >> $PROF
+#echo "[mipsel]" >> $PROF
+#cat panda_profile.mipsel >> $PROF
+#cp console.bins/* kernels/
 echo "Built by $(whoami) on $(date) at version $(git describe HEAD)" > kernels/README.txt
